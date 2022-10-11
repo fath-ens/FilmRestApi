b7f596b518597e2b8473f5699d5dda0339feb513
Fatih Enes Uslu <56679812+fath-ens@users.noreply.github.com>
2022-10-11 16:43:57 +0300

    Eski v

--- filmapp/filmapp/src/main/java/com/jpadata/filmapp/Filmm.java
+++
@@ -1,6 +0,0 @@
-package com.jpadata.filmapp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Filmm extends JpaRepository<Film, Integer> {
-}
--- filmapp/filmapp/src/main/java/com/jpadata/filmapp/Yorumm.java
+++
@@ -1,9 +0,0 @@
-package com.jpadata.filmapp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.*;
-
-public interface Yorumm extends JpaRepository<Yorum, Integer> {
-    List<Yorum> findByfid(int f_id);
-}
