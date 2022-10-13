1cc4c9aa6a2ded118d64b6754baf01df16156b4f
Fatih Enes Uslu <56679812+fath-ens@users.noreply.github.com>
2022-10-13 14:04:31 +0300

    old

--- filmapi/src/main/java/com/filmrestapi/filmapi/repository/FilmRepository.java
+++
@@ -1,7 +0,0 @@
-package com.filmrestapi.filmapi.repository;
-
-import com.filmrestapi.filmapi.entity.Film;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FilmRepository extends JpaRepository<Film, Integer> {
-}
